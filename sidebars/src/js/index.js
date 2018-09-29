26510ec086efedaf384f523518c3df4d76f21afa
Jim <jimfrenette@yahoo.com>
2018-09-29 17:38:04 -0400

    toggle sidebars at given breakpoints with javascript

--- sidebars/src/js/index.js
+++ sidebars/src/js/index.js
@@ -1,1 +1,16 @@
 import style from '../sass/style.scss'
+import Sidebar from './sidebar'
+
+new Sidebar({
+    name: 'sidebar-1',
+    input: '#sidebar-1-toggle',
+    breakpoint: '1024px',
+    exclude: []
+});
+
+new Sidebar({
+    name: 'sidebar-2',
+    input: '#sidebar-2-toggle',
+    breakpoint: '801px',
+    exclude: []
+});
